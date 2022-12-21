857acb2cf5b5cac878a8cfd1def56169b986d276
LomIv <unikalur@mail.ru>
2022-12-21 16:07:57 +0500

    OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- algorithms/2_7/2_7_1.cpp
+++ algorithms/2_7/2_7_1.cpp
@@ -1,20 +1,25 @@
+#include <iostream>
 #include <cmath>
 
 using namespace std;
 
-int n, k;
-
-cout << "Введите количество звёздочек в квадрате";
-
-cin >> n;
-
-k = sqrt(n);
-
-for (int i = 0; i < k; i++)
+int main()
 {
-    for (int j = 0; j < k; j++)
+
+    int n, k;
+
+    cout << "Введите количество звёздочек в квадрате";
+
+    cin >> n;
+
+    k = sqrt(n);
+
+    for (int i = 0; i < k; i++)
     {
-        cout << "* ";
+        for (int j = 0; j < k; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
     }
-    cout << endl;
 }
